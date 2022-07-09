47c4121679ce84ab2a48557e72e3b1ca43466543
Jacob White <jake@jwhite.network>
2022-07-09 17:48:16 -0400

    Check build output

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ ls -la
 
 cd vyos-build || exit
 
-./configure --architecture amd64 --build-by "Cyb3r Jak3 git@cyberjake.xyz"
+./configure --architecture amd64 --build-by "Cyb3r Jak3 git@cyberjake.xyz" --build-type release --build-comment "Download from https://github.com/Cyb3r-Jak3/vyos-builder"
 
 sudo make iso
