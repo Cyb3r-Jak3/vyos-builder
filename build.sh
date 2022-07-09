f4a3212ff990e1377326928981c05676746d664a
Jacob White <jake@jwhite.network>
2022-07-09 17:51:53 -0400

    Remove build comment

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ ls -la
 
 cd vyos-build || exit
 
-./configure --architecture amd64 --build-by "Cyb3r Jak3 git@cyberjake.xyz" --build-type release --build-comment "Download from https://github.com/Cyb3r-Jak3/vyos-builder"
+./configure --architecture amd64 --build-by "Cyb3r Jak3 git@cyberjake.xyz" --build-type release
 
 sudo make iso
